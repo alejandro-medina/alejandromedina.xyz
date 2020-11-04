8ec1b7b4be4d8f7aaa67a4c023a2b618b413ab33
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2020-11-04 03:47:27 -0600

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Cover from "./components/Cover.svelte";
+
+	const name = "Alejandro Medina";
+</script>
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		padding: 0 !important;
+		box-sizing: border-box;
+		font-family: "Roboto", sans-serif;
+	}
+</style>
+
+<main>
+	<Cover />
+</main>
---
+++ src/components/Cover.svelte
@@ -0,0 +1,83 @@
+<script>
+</script>
+
+<style>
+    main#Cover {    
+        height: 100vh;
+        width: 100%;
+        background: #222222;
+        color: #fafafa;
+    }
+
+    main#Cover .container, main#Cover .row, main#Cover .row .col-12 {
+        height: 100%;
+    }
+
+    div.cover-info {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    div.cover-info h1 {
+        font-size: 3em;
+    }
+
+    div.cover-info p {
+        font-size: 1.5em;
+    }
+
+    div.contact-info {
+        display:initial;
+        
+    }
+
+    div.contact-info ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+    }
+
+    div.contact-info ul a li{
+        display: inline-block;
+        margin-right: 1em;
+        padding: 10px 15px;
+        background: #fafafa;
+        cursor: pointer;
+    }
+
+    div.contact-info ul a li:hover{
+        background: #d4d4d4;
+    }
+
+    div.contact-info ul a {
+        color: #222222;
+        text-decoration: none;
+    }
+
+</style>
+
+<main id="Cover">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="cover-info">
+                    <h1>Alejandro Medina</h1>
+                    <p>Coding ideas into products, services, reality...</p>
+                    <div class="contact-info">
+                        <ul>
+                            <a target="_blank" href="https://alejandromedina.xyz"><li>Website</li></a>
+                            <a target="_blank" href="http://github.com/alejandro-medina"><li>Github</li></a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</main>
